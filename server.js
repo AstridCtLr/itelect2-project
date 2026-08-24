6b9dce9d1b943da7c79b32eddda7d026e4c53888
James Israel <tlga.jamesisraelnario@gmail.com>
2026-08-24 23:31:28 +0800

    GT7: Connect PostgreSQL via Sequelize

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan('dev')); 
 app.use(express.json());
 
 app.use('/api', router);
